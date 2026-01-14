ed5e316525035cf4ef03f72532eb4ddc9c30a024
nikita <nmkazantsev@gmail.com>
2026-01-14 18:36:37 +0300

    add desktop app as separate app

---
+++ game/src/main/java/com/nikitos/MainRenderer.java
@@ -0,0 +1,21 @@
+package com.nikitos;
+
+import com.nikitos.utils.Utils;
+
+public class MainRenderer extends GamePageClass {
+
+    @Override
+    public void draw() {
+        Utils.background(255, 100, 0);
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+}
