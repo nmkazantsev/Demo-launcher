186c3ba32dd0ef672dc961adfa8aac00acc731bd
nikita <nmkazantsev@gmail.com>
2026-01-28 18:40:32 +0300

    refactor shaders

--- game/src/main/java/com/nikitos/MainRenderer.java
+++ game/src/main/java/com/nikitos/MainRenderer.java
@@ -1,14 +1,24 @@
 package com.nikitos;
 
+import com.nikitos.main.shaders.Shader;
+import com.nikitos.main.shaders.default_adaptors.MainShaderAdaptor;
+import com.nikitos.utils.FileUtils;
 import com.nikitos.utils.Utils;
 
 public class MainRenderer extends GamePageClass {
-    public MainRenderer(){
+    private Shader shader;
 
+    public MainRenderer() {
+        FileUtils fileUtils = new FileUtils();
+        shader = new Shader(
+                fileUtils.readFileFromAssets(CoreRenderer.class, "vertex_shader.glsl"),
+                fileUtils.readFileFromAssets(CoreRenderer.class, "fragment_shader.glsl"),
+                this, new MainShaderAdaptor());
     }
 
     @Override
     public void draw() {
+        shader.apply();
         Utils.background(255, 100, 0);
     }
 
