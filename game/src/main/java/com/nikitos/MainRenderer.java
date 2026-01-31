b020b056ad1540d328106a14930c9661398a0759
nikita <nmkazantsev@gmail.com>
2026-02-01 00:07:05 +0300

    pc light works

--- game/src/main/java/com/nikitos/MainRenderer.java
+++ game/src/main/java/com/nikitos/MainRenderer.java
@@ -3,7 +3,12 @@ package com.nikitos;
 import com.nikitos.main.camera.Camera;
 import com.nikitos.main.frameBuffers.FrameBuffer;
 import com.nikitos.main.images.PImage;
+import com.nikitos.main.light.AmbientLight;
+import com.nikitos.main.light.DirectedLight;
+import com.nikitos.main.light.Material;
+import com.nikitos.main.light.SourceLight;
 import com.nikitos.main.shaders.Shader;
+import com.nikitos.main.shaders.default_adaptors.LightShaderAdaptor;
 import com.nikitos.main.shaders.default_adaptors.MainShaderAdaptor;
 import com.nikitos.main.vertices.Shape;
 import com.nikitos.main.vertices.SimplePolygon;
@@ -25,7 +30,7 @@ import static com.nikitos.utils.Utils.*;
 
 public class MainRenderer extends GamePageClass {
     private final Engine engine;
-    private final Shader shader;
+    private final Shader shader, lightShader;
     private final PlatformBridge pb;
 
     private float[] matrix = new float[16];
@@ -39,6 +44,11 @@ public class MainRenderer extends GamePageClass {
     private final Shader skyBoxShader;
 
     private final FrameBuffer fb;
+
+    private final SourceLight sourceLight;
+    private final AmbientLight ambientLight;
+    private final DirectedLight directedLight1;
+    private final Material material;
 
     public MainRenderer() {
         engine = CoreRenderer.engine;
@@ -57,6 +67,7 @@ public class MainRenderer extends GamePageClass {
 
         simplePolygon = new SimplePolygon(this::redraw, true, 0, this);
         shape = new Shape("/shape/ponchik.obj", "/shape/texture.png", this, this.getClass());
+        shape.addNormalMap("/shape/normal_tex.png");
 
         skyBox = new SkyBox("/skybox/", "jpg", this);
 
@@ -65,7 +76,47 @@ public class MainRenderer extends GamePageClass {
                 fileUtils.readFileFromAssets(this.getClass(), "/skybox/skybox_fragment.glsl"),
                 this, new MainShaderAdaptor());
 
+        lightShader= new Shader(
+                fileUtils.readFileFromAssets(this.getClass(), "/shape/vertex_shader_light.glsl"),
+                fileUtils.readFileFromAssets(this.getClass(), "/shape/fragment_shader_light.glsl"),
+                this, new LightShaderAdaptor());
+
+
         fb = new FrameBuffer((int) x, (int) Utils.y, this);
+
+        ambientLight = new AmbientLight(this);
+        // ambientLight.color = new Vec3(0.3f, 0.3f, 0.3f);
+
+        directedLight1 = new DirectedLight(this);
+        directedLight1.direction = new PVector(-1, 0, 0);
+        directedLight1.color = new PVector(0.9f);
+        directedLight1.diffuse = 0.2f;
+        directedLight1.specular = 0.8f;
+       /* directedLight2 = new DirectedLight(this);
+        directedLight2.direction = new Vec3(0, 1, 0);
+        directedLight2.color = new Vec3(0.6f);
+        directedLight2.diffuse = 0.9f;
+        directedLight2.specular = 0.8f;
+
+        */
+        sourceLight = new SourceLight(this);
+        sourceLight.diffuse = 0.8f;
+        sourceLight.specular = 0.9f;
+        sourceLight.constant = 1f;
+        sourceLight.linear = 0.01f;
+        sourceLight.quadratic = 0.01f;
+        sourceLight.color = new PVector(0.5f);
+        sourceLight.position = new PVector(2.7f, 0, 0);
+        sourceLight.direction = new PVector(-0.3f, 0, 0);
+        sourceLight.outerCutOff = cos(radians(40));
+        sourceLight.cutOff = cos(radians(30f));
+
+        material = new Material(this);
+        material.ambient = new PVector(1);
+        material.specular = new PVector(1);
+        material.diffuse = new PVector(1);
+        material.shininess = 1.1f;
+
     }
 
     @Override
@@ -79,14 +130,14 @@ public class MainRenderer extends GamePageClass {
 
         skyBox.prepareAndDraw();
 
-
-        shader.apply();
+        lightShader.apply();
+        material.apply();
         camera.apply();
         Matrix.rotateM(matrix, 0, engine.pageMillis() / 10.0f, 0, 1, 1);
         Matrix.applyMatrix(matrix);
         shape.prepareAndDraw();
         fb.connectDefaultFrameBuffer();
-
+        shader.apply();
         engine.glClear();
         camera.resetFor2d();
         camera.apply();
